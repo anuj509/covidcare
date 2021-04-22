cc18b6a1e8347faa9d80a2c12a1f676cdb7a2218
Anuj Shah <anujshah.2016@gmail.com>
2021-04-22 20:38:31 +0530

    scheduler added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,9 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
+use App\Models\Post;
+use App\Models\Feed;
+use Carbon\Carbon;
 class Kernel extends ConsoleKernel
 {
     /**
@@ -24,7 +26,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        // $schedule->command('inspire')
+        //      ->everyMinute()
+        //      ->appendOutputTo(storage_path('logs/inspire.log'));
         // $schedule->command('inspire')->hourly();
+        // $schedule->call(function () {
+        //     Post::where('created_at', '<', Carbon::now()->subDays(7))->delete();
+        // })->daily();
+        $schedule->call(function () {
+            Post::where('created_at', '<', Carbon::now()->subDays(14))->delete();
+            Feed::where('created_at', '<', Carbon::now()->subDays(14))->delete();
+        })->daily();
     }
 
     /**
