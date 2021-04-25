850f91358f85fb1da59a54321617c62004a6c92a
Anuj Shah <anujshah.2016@gmail.com>
2021-04-25 16:09:24 +0530

    google sheet code enabled

--- app/Http/Controllers/API/PostAPIController.php
+++ app/Http/Controllers/API/PostAPIController.php
@@ -139,36 +139,36 @@ class PostAPIController extends AppBaseController
         // dd($input);
         $post = $this->postRepository->create($input);
         
-        // $client = $this->getClient();
-        // $this->service = new Google_Service_Sheets($client);
-        // $values = [
-        //     [
-        //         $post['name'],
-        //         $post['dob'],
-        //         $post['gender'],
-        //         $post['blood_group'],
-        //         $post['oxygen_level'],
-        //         $post['poc_name'],
-        //         $post['poc_phone'],
-        //         $post['patient_currently_admitted_at'],
-        //         $post['ward'],
-        //         $post['requirement'],
-        //         $post['oxygen'],
-        //         $post['plasma'],
-        //         $post['medicines'],
-        //         $post['bed'],
-        //         $post['other'],
-        //         $post['created_at']
-        //     ],
-        // ];
-        // $body = new Google_Service_Sheets_ValueRange([
-        //     'values' => $values
-        // ]);
-        // $valueInputOption = "RAW";
-        // $params = [
-        //     'valueInputOption' => $valueInputOption
-        // ];
-        // $result = $this->service->spreadsheets_values->append(env('GOOGLE_SHEET_ID'), 'Sheet1', $body, $params);
+        $client = $this->getClient();
+        $this->service = new Google_Service_Sheets($client);
+        $values = [
+            [
+                $post['name'],
+                $post['dob'],
+                $post['gender'],
+                $post['blood_group'],
+                $post['oxygen_level'],
+                $post['poc_name'],
+                $post['poc_phone'],
+                $post['patient_currently_admitted_at'],
+                $post['ward'],
+                $post['requirement'],
+                $post['oxygen'],
+                $post['plasma'],
+                $post['medicines'],
+                $post['bed'],
+                $post['other'],
+                $post['created_at']
+            ],
+        ];
+        $body = new Google_Service_Sheets_ValueRange([
+            'values' => $values
+        ]);
+        $valueInputOption = "RAW";
+        $params = [
+            'valueInputOption' => $valueInputOption
+        ];
+        $result = $this->service->spreadsheets_values->append(env('GOOGLE_SHEET_ID'), 'Sheet1', $body, $params);
         
         return $this->sendResponse(new PostResource($post), 'Post saved successfully');
     }
@@ -318,33 +318,33 @@ class PostAPIController extends AppBaseController
             $client->setAccessToken($accessToken);
         }
 
-        // If there is no previous token or it's expired.
-        if ($client->isAccessTokenExpired()) {
-            // Refresh the token if possible, else fetch a new one.
-            if ($client->getRefreshToken()) {
-                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            } else {
-                // Request authorization from the user.
-                $authUrl = $client->createAuthUrl();
-                printf("Open the following link in your browser:\n%s\n", $authUrl);
-                print 'Enter verification code: ';
-                $authCode = trim(fgets(STDIN));
+        // // If there is no previous token or it's expired.
+        // if ($client->isAccessTokenExpired()) {
+        //     // Refresh the token if possible, else fetch a new one.
+        //     if ($client->getRefreshToken()) {
+        //         $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        //     } else {
+        //         // Request authorization from the user.
+        //         $authUrl = $client->createAuthUrl();
+        //         printf("Open the following link in your browser:\n%s\n", $authUrl);
+        //         print 'Enter verification code: ';
+        //         $authCode = trim(fgets(STDIN));
 
-                // Exchange authorization code for an access token.
-                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-                $client->setAccessToken($accessToken);
+        //         // Exchange authorization code for an access token.
+        //         $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+        //         $client->setAccessToken($accessToken);
 
-                // Check to see if there was an error.
-                if (array_key_exists('error', $accessToken)) {
-                    throw new Exception(join(', ', $accessToken));
-                }
-            }
-            // Save the token to a file.
-            if (!file_exists(dirname($tokenPath))) {
-                mkdir(dirname($tokenPath), 0700, true);
-            }
-            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-        }
+        //         // Check to see if there was an error.
+        //         if (array_key_exists('error', $accessToken)) {
+        //             throw new Exception(join(', ', $accessToken));
+        //         }
+        //     }
+        //     // Save the token to a file.
+        //     if (!file_exists(dirname($tokenPath))) {
+        //         mkdir(dirname($tokenPath), 0700, true);
+        //     }
+        //     file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        // }
         return $client;
     }
 }
