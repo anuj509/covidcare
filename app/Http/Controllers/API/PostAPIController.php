4274d88fcee7f813334f453aab226abaf27728a5
Anuj Shah <anujshah.2016@gmail.com>
2021-04-25 12:53:11 +0530

    google sheet

--- app/Http/Controllers/API/PostAPIController.php
+++ app/Http/Controllers/API/PostAPIController.php
@@ -13,6 +13,10 @@ use App\Http\Controllers\AppBaseController;
 use App\Http\Resources\PostResource;
 use Response;
 use Carbon\Carbon;
+// use App\Jobs\SaveToSheetJob;
+use Google_Client;
+use Google_Service_Sheets;
+use Google_Service_Sheets_ValueRange;
 /**
  * Class PostController
  * @package App\Http\Controllers\API
@@ -134,6 +138,38 @@ class PostAPIController extends AppBaseController
         $input['comment']="";
         // dd($input);
         $post = $this->postRepository->create($input);
+
+        // SaveToSheetJob::dispatch($this->postRepository,$post->id);
+        $client = $this->getClient();
+        $this->service = new Google_Service_Sheets($client);
+        $values = [
+            [
+                $post['name'],
+                $post['dob'],
+                $post['gender'],
+                $post['blood_group'],
+                $post['oxygen_level'],
+                $post['poc_name'],
+                $post['poc_phone'],
+                $post['patient_currently_admitted_at'],
+                $post['ward'],
+                $post['requirement'],
+                $post['oxygen'],
+                $post['plasma'],
+                $post['medicines'],
+                $post['bed'],
+                $post['other'],
+                $post['created_at']
+            ],
+        ];
+        $body = new Google_Service_Sheets_ValueRange([
+            'values' => $values
+        ]);
+        $valueInputOption = "RAW";
+        $params = [
+            'valueInputOption' => $valueInputOption
+        ];
+        $result = $this->service->spreadsheets_values->append(env('GOOGLE_SHEET_ID'), 'Sheet1', $body, $params);
         
         return $this->sendResponse(new PostResource($post), 'Post saved successfully');
     }
@@ -258,5 +294,58 @@ class PostAPIController extends AppBaseController
         }
 
         return $this->sendResponse($counter, 'Stats retrieved successfully');
+    }
+
+    /**
+     * Returns an authorized API client.
+     * @return Google_Client the authorized client object
+     */
+    public function getClient()
+    {
+        $client = new Google_Client();
+        $client->setApplicationName('CovidCare Data Ingestor');
+        $client->setScopes(Google_Service_Sheets::SPREADSHEETS);
+        $client->setAuthConfig(storage_path('credentials.json'));
+        $client->setAccessType('offline');
+        $client->setPrompt('none');
+
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = storage_path('token.json');
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("Open the following link in your browser:\n%s\n", $authUrl);
+                print 'Enter verification code: ';
+                $authCode = trim(fgets(STDIN));
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
     }
 }
