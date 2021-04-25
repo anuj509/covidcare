9e21830400e24c626080afce57d6912db0702fa9
Anuj Shah <anujshah.2016@gmail.com>
2021-04-25 16:39:53 +0530

    google sheet code fix

--- app/Http/Controllers/API/PostAPIController.php
+++ app/Http/Controllers/API/PostAPIController.php
@@ -322,33 +322,34 @@ class PostAPIController extends AppBaseController
             $client->setAccessToken($accessToken);
         }
 
-        // // If there is no previous token or it's expired.
-        // if ($client->isAccessTokenExpired()) {
-        //     // Refresh the token if possible, else fetch a new one.
-        //     if ($client->getRefreshToken()) {
-        //         $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-        //     } else {
-        //         // Request authorization from the user.
-        //         $authUrl = $client->createAuthUrl();
-        //         printf("Open the following link in your browser:\n%s\n", $authUrl);
-        //         print 'Enter verification code: ';
-        //         $authCode = trim(fgets(STDIN));
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } 
+            // else {
+            //     // Request authorization from the user.
+            //     $authUrl = $client->createAuthUrl();
+            //     printf("Open the following link in your browser:\n%s\n", $authUrl);
+            //     print 'Enter verification code: ';
+            //     $authCode = trim(fgets(STDIN));
 
-        //         // Exchange authorization code for an access token.
-        //         $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-        //         $client->setAccessToken($accessToken);
+            //     // Exchange authorization code for an access token.
+            //     $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            //     $client->setAccessToken($accessToken);
 
-        //         // Check to see if there was an error.
-        //         if (array_key_exists('error', $accessToken)) {
-        //             throw new Exception(join(', ', $accessToken));
-        //         }
-        //     }
-        //     // Save the token to a file.
-        //     if (!file_exists(dirname($tokenPath))) {
-        //         mkdir(dirname($tokenPath), 0700, true);
-        //     }
-        //     file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-        // }
+            //     // Check to see if there was an error.
+            //     if (array_key_exists('error', $accessToken)) {
+            //         throw new Exception(join(', ', $accessToken));
+            //     }
+            // }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0777, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
         return $client;
     }
 }
